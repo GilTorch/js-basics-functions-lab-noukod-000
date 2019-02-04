d0ac0b0daae5deb8c82d179a7648e6605956a128
Gilbert Torchon <gilbert@noukod.com>
2019-02-04 19:44:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function calculatesFarePrice(startBlock,endBlock){
    const d = distanceTravelledInFeet(startBlock,endBlock);
    let fare=0
    if(d>400 && d<=2000){
-     flare=d*2
+     fare=d*2
    }else if(d >= 2000){
      flare=d*25
    }
